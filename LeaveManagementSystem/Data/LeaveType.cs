918d64f5282a27a502d4623e84a1b93526517f90
JIASHI0330 <jiaswang@student.unimelb.edu.au>
2024-09-28 22:18:26 +1000

    add the database of the system

---
+++ LeaveManagementSystem/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeaveManagementSystem.Data;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int NumberOfDays { get; set; }
+
+}
